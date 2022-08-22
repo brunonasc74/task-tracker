4e03ae3545f6948ae3d429b8f2d5cef2d3868013
brunonasc74 <bruno.hpn74@gmail.com>
2022-08-22 16:42:49 -0300

    Button on Header now shows and hide form on click

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,21 @@
 import PropTypes from 'prop-types';
 import Button from './Button';
 
-const Header = ({ title }) => {
-	const onClick = () => console.log('click');
-
+const Header = ({ title, onAdd, showAdd }) => {
 	return (
 		<header className='header'>
 			<h1>{title}</h1>
-			<Button onClick={onClick} color='green' text='Add' />
+			<Button
+				color={showAdd ? '#fa3030' : 'green'}
+				text={showAdd ? 'Close' : 'Add'}
+				onClick={onAdd}
+			/>
 		</header>
 	);
 };
 
 Header.defaultProps = {
-	title: 'default title'
+	title: 'Task Tracker'
 };
 
 Header.propTypes = {
